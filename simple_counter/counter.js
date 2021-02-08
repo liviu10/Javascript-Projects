de7e1a6003117b9f63043120449c3effd2d1eda6
Liviu Voica <voica.liviu10@gmail.com>
2021-02-08 15:31:54 +0200

    Renaming all the folders to something more accessible

---
+++ simple_counter/counter.js
@@ -0,0 +1,34 @@
+(function(){
+  const buttons = document.querySelectorAll('.btn')
+  let count = 0
+
+
+  //Add event listeners and functionality to each button  
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(){
+      if (button.classList.contains('prevBtn')){
+        count--
+      } else if (button.classList.contains('nextBtn')){
+        count++
+      } else if (button.classList.contains('resetBtn')) {
+        count = 0;
+      } else if (button.classList.contains('prev5Btn')) {
+        count -= 5;
+      } else if (button.classList.contains('next5Btn')) {
+        count += 5;
+      }
+
+      //Select the counter text
+      const counter = document.querySelector('#counter')
+      counter.textContent = count
+
+      if (count < 0 ){
+        counter.style.color = '#FF0000'
+      } else if (count > 0){
+        counter.style.color = '#00FF00'
+      } else {
+        counter.style.color = '#000000'
+      }
+    })
+  })
+})()
