4680e7b2e3a4b3aea395b27159a31431daa2a094
Liviu Voica <voica.liviu10@gmail.com>
2021-02-08 15:38:23 +0200

    Renaming all the subdirectories from the root folder to something more accessible

--- Change Background Color/change_background.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("button");
-const body = document.querySelector("body");
-const colors = ["red", "green", "blue", "yellow", "pink", "purple"];
-
-button.addEventListener("click", function changeBackground() {
-  const colorIndex = parseInt(Math.random()*colors.length);
-  body.style.backgroundColor = colors[colorIndex];
-});
